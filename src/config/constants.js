2fdf97103c7043cff604cb450ce657313e92965d
Hennessia <Hennessia@yandex.ru>
2023-01-28 17:10:48 +0300

    Updates

--- src/config/constants.js
+++
@@ -1,3 +0,0 @@
-export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
-export const SHORTMOVIE_LIMIT = 40;
-export const MOVIES_MY_API_URL = 'https:/hennessia.ru/';
--- src/config/regExp.js
+++
@@ -1,3 +0,0 @@
-export const userNameRegExp = "^[A-Za-zА-Яа-яёЁ]+(?:[- ][A-Za-zА-Яа-яёЁ]+)*$"
-// export const emailRegExp = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-export const emailRegExp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
